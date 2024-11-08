c34baf45368219cb46720e6e07cba8ff4a9b134d
Josefrank90 <josefrank218@gmail.com>
2024-11-08 10:27:21 -0600

    implemente codigos al login para que sea funcionable

--- src/app/components/body/login/login.component.ts
+++ src/app/components/body/login/login.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  username: string = '';
+  password: string = '';
 
+  onSubmit() {
+    if (this.username && this.password) {
+      console.log('Usuario:', this.username);
+      console.log('Contraseña:', this.password);
+      alert('Inicio de sesión exitoso'); 
+    } else {
+      alert('Por favor, ingrese usuario y contraseña');
+}
+
+  }
 }
